b23945e5f9c72e798f96b14c7f69af01a0b38a83
Kasi Reeves <Kasirocswell@Rocketmail.com>
2022-08-30 01:56:07 -0700

    final change

--- my-app/components/Cards.jsx
+++ my-app/components/Cards.jsx
@@ -2,7 +2,7 @@ export default function Cards() {
     return (
         <div className="border border-black shadow-xl flex flex-col bg-gradient-to-br from-[#5498D8] to-[#3E71AC] w-[80%] h-[300px] mx-auto rounded-xl lg:w-[400px] lg:mt-[25px]">
             <h2 className="mt-[10px] font-abril text-xl mx-auto text-center">Tired of Bots?</h2>
-            <p className="flex mx-[10px] text-center font-prompt mt-[10px]">Bots are one of the biggest threats to a fair online lottery.  Unlike the top shoe lottery apps available today, Nice Shoes takes steps to ensure that bots are not being used to win.  Because our games have limited capacity and require a fee, using bots would cost more money than the actual prize is worth, making bots uselss in our lottery games.</p>
+            <p className="flex mx-[10px] text-center font-prompt mt-[10px]">Bots are one of the biggest threats to a fair online lottery.  Unlike the top shoe lottery apps available today, Nice Shoes takes steps to ensure that bots are not being used to win.  Because our games have limited capacity and require a fee, using bots would cost more money than the actual prize is worth, making bots uselss in our lottery.</p>
         </div>
     )
 }
