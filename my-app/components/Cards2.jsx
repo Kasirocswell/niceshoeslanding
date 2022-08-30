740b95e2bc9ab72d551a48a0479fb13ed895bfc5
Kasi Reeves <Kasirocswell@Rocketmail.com>
2022-08-30 02:11:51 -0700

    final change for real

--- my-app/components/Cards2.jsx
+++ my-app/components/Cards2.jsx
@@ -2,7 +2,7 @@ export default function Cards2() {
     return (
         <div className="border border-black shadow-xl flex flex-col bg-gradient-to-br from-[#5498D8] to-[#3E71AC] w-[80%] h-[300px] mx-auto rounded-xl mt-[25px] lg:w-[400px]">
             <h2 className="mt-[10px] font-abril text-xl mx-auto text-center">Feeling Like You Never Win?</h2>
-            <p className="flex mx-[10px] text-center font-prompt mt-[10px]">Instead of competeting with hundreds and sometimes even hundreds of thousands of contestants, our games cap the amount of players that can enter the game.  Our games range between 100 and 1000 contestants but never more or less.  This way you can feel confident that your chances of winning are higher than ever.</p>
+            <p className="flex mx-[10px] text-center font-prompt mt-[10px]">Instead of competing with hundreds and sometimes even hundreds of thousands of contestants, our games cap the amount of players that can enter the game.  Our games range between 100 and 1000 contestants but never more or less.  This way you can feel confident that your chances of winning are higher than ever.</p>
         </div> 
     )
 }
